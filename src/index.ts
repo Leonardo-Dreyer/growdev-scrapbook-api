3032fa93be3e1967a9de4a54d369dad874471e5c
Leonardo Araújo Dreyer <leonardodreyer99@gmail.com>
2022-02-23 21:32:47 -0300

    add rota  get

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import express, { Request, Response } from 'express';
+import express, { Request, response, Response } from 'express';
 import cors from 'cors';
 import { request } from 'http';
 
@@ -6,6 +6,12 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
+
+app.get('./users', (request: Request, response: Response) => {
+    return response.json({
+        menssage: 'Olá, mundo'
+    });
+});
 
 const port = process.env.PORT || 8080;
 app.listen(port, () => {
