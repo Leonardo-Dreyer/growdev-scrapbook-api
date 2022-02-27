dc9789855aefbaa37f6f2a1e5fbcb24dba66eabd
Leonardo Araújo Dreyer <leonardodreyer99@gmail.com>
2022-02-27 20:33:16 -0300

    change names

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,6 @@ import express, { NextFunction, Request, Response } from 'express';
 import cors from 'cors';
 import Messages from './classes/Messages';
 import User from './classes/User';
-import { request } from 'http';
 
 const app = express();
 app.use(express.json());
@@ -11,7 +10,7 @@ app.use(cors());
 
 const users: User[] = [];
 
-const validatUser = function (request: any, response: any, next: NextFunction) {
+function validatUser(request: any, response: any, next: NextFunction) {
     const { name, password, repeatPassword, logged } = request.body;
     if (!name || name.length < 3 || !password || password.length < 3 || password !== repeatPassword) {
         return response.sendStatus(400).json();
@@ -29,40 +28,6 @@ const validatUser = function (request: any, response: any, next: NextFunction) {
     };
 };
 
-app.post('/users', validatUser, (request: Request, response: Response) => {
-    const { name, password, repeatPassword, logged } = request.body;
-    const user = new User(
-        name,
-        parseInt(password),
-        parseInt(repeatPassword),
-        logged
-    );
-    users.push(user);
-    console.log(users);
-    return response.status(201).json({ message: 'Usário cadastrado com sucesso!' });
-});
-
-function saveMessage(request: any, response: Response, next: NextFunction) {
-    const { descrition, detailing } = request.body;
-    users.forEach(user => {
-        if (user.logged === true) {
-            const message = new Messages(descrition, detailing);
-            request.id = message.id;
-            user.messages.push(message);
-            next();
-        };
-    });
-}
-
-app.post('/users/messages', saveMessage, (request: any, response: Response) => {
-    const { descrition, detailing } = request.body;
-    return response.status(201).json({
-        id: request.id,
-        descrition: descrition,
-        detailing: detailing
-    });
-});
-
 function validateLogin(request: Request, response: Response, next: NextFunction) {
     const { name, password } = request.params;
     users.forEach(user => {
@@ -74,17 +39,36 @@ function validateLogin(request: Request, response: Response, next: NextFunction)
     return response.status(404).json();
 };
 
-app.get('/users/:name/password/:password', validateLogin, (request: Request, response: Response) => {
-    return response.status(202).json();
-});
-
-app.get('/users/logout', (request: Request, response: Response) => {
+function saveMessages(request: any, response: Response, next: NextFunction) {
+    const { descrition, detailing } = request.body;
     users.forEach(user => {
         if (user.logged === true) {
-            user.logged = false;
-            console.log(users);
-            return response.status(202).json();
+            const message = new Messages(descrition, detailing);
+            request.id = message.id;
+            user.messages.push(message);
+            next();
         };
+    });
+};
+
+app.post('/users', validatUser, (request: Request, response: Response) => {
+    const { name, password, repeatPassword, logged } = request.body;
+    const user = new User(
+        name,
+        parseInt(password),
+        parseInt(repeatPassword),
+        logged
+    );
+    users.push(user);
+    return response.status(201).json({ message: 'Usário cadastrado com sucesso!' });
+});
+
+app.post('/users/messages', saveMessages, (request: any, response: Response) => {
+    const { descrition, detailing } = request.body;
+    return response.status(201).json({
+        id: request.id,
+        descrition: descrition,
+        detailing: detailing
     });
 });
 
@@ -104,6 +88,20 @@ app.get('/users/messages/:id', (request: Request, response: Response) => {
                 return response.json(message);
             };
         });
+    });
+});
+
+app.put('/users/:name/password/:password', validateLogin, (request: Request, response: Response) => {
+    return response.status(202).json();
+    
+});
+
+app.put('/users', (request: Request, response: Response) => {
+    users.forEach(user => {
+        if (user.logged === true) {
+            user.logged = false;
+            return response.status(202).json();
+        };
     });
 });
 
@@ -134,6 +132,7 @@ app.delete('/users/messages/:id', (request: Request, response: Response) => {
     });
 });
 
-app.listen(8080, () => {
-    console.log('API running...');
-});       
+const port = process.env.PORT || 8080;
+app.listen(port, () => {
+    console.log(`API running on port ${port}.`);
+});  
