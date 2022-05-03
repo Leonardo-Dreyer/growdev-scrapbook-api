0f8e42623b94a55926e1e7d3200abd0c1bf98767
leonardo-growdev <leonardodreyer99@gmail.com>
2022-05-03 14:17:31 -0300

    Ajuste ormconfig caminhos dinamicos

--- ormconfig.js
+++ ormconfig.js
@@ -1,6 +1,8 @@
+const path = require('path');
+
 require('dotenv').config();
 
-module.exports  = {
+module.exports = {
     type: 'postgres',
     url: process.env.DATABASE_URL,
     synchronize: false,
@@ -10,17 +12,10 @@ module.exports  = {
             rejectUnauthorized: false
         }
     },
-    entities: [
-        'src/core/data/database/entities/**/*'
-    ],
-    migrations: [
-        'src/core/data/database/migrations/**/*'
-      ],
+    entities: [path.join(__dirname, '..', 'entities', '**', '*')],
+    migrations: [path.join(__dirname, 'migrations', '**', '*')],
     cli: {
         entitiesDir: 'src/core/data/database/entities',
         migrationsDir: 'src/core/data/database/migrations'
-    }  
-}
-
-
-
+    }
+};
