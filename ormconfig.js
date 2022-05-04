fcec4259dea599434baac4fa89fd22efb7e8024e
leonardo-growdev <leonardo.araujo@growdev.com.br>
2022-05-03 21:17:39 -0300

    teste ormconfig caminho

--- ormconfig.js
+++ ormconfig.js
@@ -12,12 +12,12 @@ module.exports = {
             rejectUnauthorized: false
         }
     },
-    migrations: ['src/core/data/database/migrations/**/*'],
-    entities: ['src/core/data/database/entities/**/*'],
-    entities: [path.join(__dirname, '..', 'entities', '**', '*')],
-    migrations: [path.join(__dirname, 'migrations', '**', '*')],
+    migrations: ['src/database/migrations/**/*'],
+    entities: ['src/database/entities/**/*'],
+    /* entities: [path.join(__dirname, '..', 'entities', '**', '*')],
+    migrations: [path.join(__dirname, 'migrations', '**', '*')], */
     cli: {
-        entitiesDir: 'src/core/data/database/entities',
-        migrationsDir: 'src/core/data/database/migrations'
+        entitiesDir: 'src/database/entities',
+        migrationsDir: 'src/database/migrations'
     }
 };
