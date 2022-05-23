046cd20aa6443b2b7837a787891b064ecb7070e4
leonardo-growdev <leonardo.araujo@growdev.com.br>
2022-05-22 23:16:27 -0300

    Ajuste conexão redis

--- src/database/connections/Redis.ts
+++ src/database/connections/Redis.ts
@@ -17,7 +17,11 @@ export default class Redis {
 
     private openConnection() {
         try {
-            return new IORedis();
+            return new IORedis(process.env.REDIS_URL, {
+                tls: {
+                    rejectUnauthorized: false
+                }
+            });
         } catch (error) {
             throw new Error(`Erro ao conectar no Redis: ${error}`);
         }
