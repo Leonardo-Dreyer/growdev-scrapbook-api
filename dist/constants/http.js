cd86e428dc6098260a9f9a8521981abee8a5e52b
Leonardo Dreyer <leonardo.araujo@growdev.com.br>
2022-06-17 21:24:15 -0300

    Remove dist

--- dist/constants/http.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.HttpInternalErrorCode = exports.HttpUnauthorizedCode = exports.HttpBadRequestCode = exports.HttpSuccessCode = void 0;
-exports.HttpSuccessCode = 200;
-exports.HttpBadRequestCode = 400;
-exports.HttpUnauthorizedCode = 401;
-exports.HttpInternalErrorCode = 500;
--- dist/constants/messages.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.invalidPassword = exports.invalidUser = exports.invalidField = exports.defaultErrorMessage = void 0;
-exports.defaultErrorMessage = 'Ocorreu um erro, tente novamente mais tarde!';
-const invalidField = (field) => `${field} inválido!`;
-exports.invalidField = invalidField;
-exports.invalidUser = 'Usuário inválido!';
-exports.invalidPassword = 'Senha incorreta!';
